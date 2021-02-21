02dded6f277520bafc7aa62d1d0eb25476c6bc1a
Lucas Valadão Bispo <64443466+LucasValada@users.noreply.github.com>
2021-02-21 17:29:57 -0300

    Delete dobrotriploraizquadrada.py

--- dobrotriploraizquadrada.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input("digite um número"))
-print("o dobro desse número é {}! ".format(n1+n1))
-print("o triplo desse número é{}! ".format(n1+n1+n1))
-print("a raiz quadrada desse número é{}! ".format(n1**0.5))
-
