0a898d5cc7a05c7c9562d27414f6f956a8c061e2
Lucas Valadão Bispo <64443466+LucasValada@users.noreply.github.com>
2021-02-21 17:29:34 -0300

    Delete calculandodescontos.py

--- calculandodescontos.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input("Qual o preço do produto?"))
-x = n1-(n1 * 5 / 100)
-print("O produto que custava R${:.2f}, na promoção está com desconto de 5%, vai custar R${:.2f} ".format(n1, x))
-
